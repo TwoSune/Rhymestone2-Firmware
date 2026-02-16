cf9b21c90bb5d3c4de688f2463865a3ed9585f0f
TwoSune <146044579+TwoSune@users.noreply.github.com>
2026-02-15 23:56:44 -0700

    Add files via upload

---
+++ keymaps/vial/config.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x51, 0x48, 0x35, 0xC7, 0x99, 0xA6, 0x67, 0x4B}
+
+#define VIAL_UNLOCK_COMBO_ROWS {6, 6}
+#define VIAL_UNLOCK_COMBO_COLS {3, 1}
+
+#define NO_MUSIC_MODE
+
+//#define MASTER_LEFT
+#define MASTER_RIGHT
+
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_USB_DETECT
